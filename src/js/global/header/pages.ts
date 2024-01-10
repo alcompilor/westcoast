789ff8f435794059a39a2869d6e7839bdcaf8813
Ahmed Abbasi <alcompilor@gmail.com>
2024-01-10 21:30:11 +0100

    Store navbar link - Added pages.ts to store navbar links as object

---
+++ src/js/global/header/pages.ts
@@ -0,0 +1,21 @@
+const pages: NavLinks[] = [
+    {
+        name: "Home",
+        url: "./index.html"
+    },
+        {
+        name: "Popular Courses",
+        url: "./popular.html"
+    },
+    {
+        name: "My Account",
+        url: "./account.html"
+    },
+]
+
+interface NavLinks {
+    name: string;
+    url: string;
+}
+
+export { pages };
