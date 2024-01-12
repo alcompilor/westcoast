f937e101d1ed86aca32fcefda61f208d9f2cfd96
Ahmed Abbasi <alcompilor@gmail.com>
2024-01-12 02:06:57 +0100

    Add interface for course object TypeScript support

---
+++ src/js/interfaces/course.ts
@@ -0,0 +1,25 @@
+interface Course {
+    id: Number;
+    courseTitle: string;
+    courseCode: string;
+    desc: string;
+    durationInDays: Number;
+    availability: {
+        classroom: boolean;
+        online: boolean;
+        [key: string]: boolean;
+    };
+    image: {
+        url: string;
+        desc: string;
+    };
+    scheduledDate: string;
+    bookingOptions: {
+        classroom: boolean;
+        online: boolean;
+    };
+    featured: boolean;
+    [key: string]: string | object | boolean;
+}
+
+export default Course;
