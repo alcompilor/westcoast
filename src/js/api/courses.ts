4beabe7f591684f2b4685966e86c3b744f09b740
Ahmed Abbasi <alcompilor@gmail.com>
2024-01-12 02:05:52 +0100

    Add API module to fetch courses - Added courses.ts API module to fetch courses from json-server db

---
+++ src/js/api/courses.ts
@@ -0,0 +1,12 @@
+import Course from "../interfaces/course";
+
+const reqCourses = async (): Promise<Course[]> => {
+    const res = await fetch("http://localhost:3000/courses");
+    if (!res.ok) {
+        console.error("Catalog failed to fetch courses!");
+    }
+    const data = await res.json();
+    return data;
+}
+
+export { reqCourses };
