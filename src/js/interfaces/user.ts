b5de299ebf942da92b5a98ed3c5f1bfa0012b286
Ahmed Abbasi <alcompilor@gmail.com>
2024-01-13 01:57:31 +0100

    Add new interface to support user objects fetched from db

---
+++ src/js/interfaces/user.ts
@@ -0,0 +1,11 @@
+interface User {
+    id: string;
+    fullName: string;
+    email: string;
+    password: string;
+    address: string;
+    phone: string;
+    isAdmin: boolean;
+}
+
+export default User;
