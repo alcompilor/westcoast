bf1cb590e8a117b4f412909dc5c7652689b0227c
Ahmed Abbasi <alcompilor@gmail.com>
2024-01-13 01:59:35 +0100

    Add page to render EnrollConfirm component w data

---
+++ src/js/pages/enroll/Enroll.ts
@@ -0,0 +1,30 @@
+import User from "../../interfaces/user";
+import Course from "../../interfaces/course";
+import * as CourseAPI from "../../api/courses";
+import * as UserAPI from "../../api/users";
+import * as EnrollConfirm from "../../components/enroll/EnrollConfirm";
+
+const render = async (): Promise<void> => {
+    const root: HTMLDivElement = document.querySelector(".root")!;
+
+    const userId: number = Number(localStorage.getItem("wcUserId"));
+
+    if (!userId) {
+        window.location.replace("/account.html");
+    }
+
+    const params = new URLSearchParams(window.location.search);
+    const courseID: number = Number(params.get("id"));
+
+    const courseObj: Course = await CourseAPI.reqCourse(courseID);
+    const userObj: User = await UserAPI.reqUser(userId);
+
+    const eConfirm: HTMLDivElement = EnrollConfirm.buildComponent(
+        courseObj,
+        userObj
+    );
+
+    root?.appendChild(eConfirm);
+};
+
+render();
